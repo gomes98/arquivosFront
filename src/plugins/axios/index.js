1b21f85521f6cecbab2226efe3e793d62ee5c9f6
Mauricio <gomes98@gmail.com>
2021-05-01 18:28:46 -0300

    Add CFG para build

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -1,16 +1,8 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-if (process.env.NODE_ENV === 'production') {
-    axios.defaults.baseURL = `${window.location.href}api/`
-} else {
-    axios.defaults.baseURL = 'http://localhost:8000/api/'
-}
-// axios.defaults.baseURL = 'https://empresamaissegura.stv.com.br/api/'
-
-// const success = res => res
-// const error = res => res
-// axios.interceptors.response.use(success, error)
+axios.defaults.baseURL = `${window.location.href}api/`
+//     axios.defaults.baseURL = 'http://localhost:8000/api/'
 
 Vue.use({
     install(Vue) {
--- src/plugins/socketio/index.js
+++ src/plugins/socketio/index.js
@@ -2,11 +2,8 @@ import Vue from 'vue'
 import { io } from "socket.io-client";
 
 let socket;
-if (process.env.NODE_ENV === 'production') {
-    socket = io();
-} else {
-    socket = io('http://localhost:8000');
-}
+socket = io();
+// socket = io('http://localhost:8000');
 
 Vue.use({
     install(Vue) {
